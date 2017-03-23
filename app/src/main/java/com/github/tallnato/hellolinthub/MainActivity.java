c5293f4a9355ae6e1508e71bb11fbeb5b33fe4fa
Renato Almeida <renato.m.alm@gmail.com>
2017-03-23 00:54:45 +0000

    Codacy code style test

--- app/src/main/java/com/github/tallnato/hellolinthub/MainActivity.java
+++ app/src/main/java/com/github/tallnato/hellolinthub/MainActivity.java
@@ -12,7 +12,8 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -36,17 +37,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if(id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+
+
     }
 }
